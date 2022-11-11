b165b3047cbefec864d4d2c5e4f268a2de37723a
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-11-11 14:50:27 -0500

    added sort logic with reprint

--- js/requests.js
+++ js/requests.js
@@ -8,7 +8,7 @@ function emptyClean() {
 			else if (typeof f.title === `undefined` || typeof f.genre === `undefined` || typeof f.rating === `undefined` || typeof f.director === `undefined` || typeof f.plot === `undefined`){
 				return f;
 			}
-			else if(f.genre.length < 2 || typeof f.genre === `null` ){
+			else if(f.genre.length < 2 ){
 				return f;
 			}
 		}))
@@ -27,4 +27,85 @@ function emptyClean() {
 			console.log(`all record deleted successfully`, response);
 		})
 		.catch(error => console.error(error)); /* handle errors */
+}
+
+function updateEventHandler(){
+	$(`.click-me`).click(function (event){
+		currentID =  $(this).attr('id');
+	});
+}
+
+function printData(data, style){
+	if (style === `alpha`){
+		data = data.sort((a,b) => {
+			if (a.title.toUpperCase() < b.title.toUpperCase()){
+				return -1;
+			}
+			if (a.title.toUpperCase() > b.title.toUpperCase()){
+				return 1;
+			}
+			return 0;
+		})
+	}
+	if (style === `alphaR`){
+		data = data.sort((a,b) => {
+			if (a.title.toUpperCase() < b.title.toUpperCase()){
+				return 1;
+			}
+			if (a.title.toUpperCase() > b.title.toUpperCase()){
+				return -1;
+			}
+			return 0;
+		})
+	}
+	if (style === `rating`){
+		data = data.sort((a,b) => {
+			if (a.rating < b.rating){
+				return -1;
+			}
+			if (a.rating > b.rating){
+				return 1;
+			}
+			return 0;
+		})
+	}
+	if (style === `ratingR`){
+		data = data.sort((a,b) => {
+			if (a.rating < b.rating){
+				return 1;
+			}
+			if (a.rating > b.rating){
+				return -1
+			}
+			return 0;
+		})
+	}
+	data.forEach(data => {
+		let title = data.title;
+		let genre = data.genre;
+		let rating = data.rating;
+		let director = data.director;
+		let plot = data.plot;
+			$(`#reviewBlock`).append(`<div class="row click-me" id="${data.id}">
+											<div class="col-7">
+												<div class="row">
+													${title}
+												</div>
+												<div class="row">
+													${plot}
+												</div>
+												<div class="row">
+													${genre}
+												</div>
+												<div class="row">
+													${director}
+												</div>
+											</div>
+											<div class="col">
+													${rating}
+											</div>
+										</div>`)
+
+	})
+	return data;
 }
